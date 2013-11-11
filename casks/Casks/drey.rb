bac622fceeaa826a09d10f891314df2c4b9d31d6
David Rey <david.rey.acosta@gmail.com>
2013-11-12 00:20:39 +0100

    Removed casks

--- casks/Casks/drey.rb
+++
@@ -1,7 +0,0 @@
-class Zterm < Cask
-  url 'http://www.dalverson.com/zterm/ZTerm1.2.dmg'
-  homepage 'http://www.dalverson.com/zterm/'
-  version '1.2'
-  sha1 '7d350dac27eaa7f39d676f933e64515d55bc3844'
-  link 'ZTerm.app'
-end
